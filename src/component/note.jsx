2740abae1f6d9c9460946e46bb513d938ca3d091
Goureesh Chandra <120293245+Herogo3241@users.noreply.github.com>
2023-06-21 22:01:53 +0530

    Add files via upload

---
+++ src/component/note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>LOREM IPSUM</h1>
+      <p>
+        Lorem ipsum is placeholder text commonly used in the graphic, print, and
+        publishing industries for previewing layouts and visual mockups.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
